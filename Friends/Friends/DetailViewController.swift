9588f688f476173d42bff6090f8eb6d989732f53
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-31 13:31:50 -0800

    Setup storyboard and made connections.

---
+++ Friends/Friends/DetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailViewController.swift
+//  Friends
+//
+//  Created by Nathanael Youngren on 1/31/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var imageOfFriend: UIImageView!
+    @IBOutlet weak var friendNameLabel: UILabel!
+    @IBOutlet weak var friendDetailLabel: UILabel!
+    
+}
---
+++ Friends/Friends/FriendTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  FriendTableViewCell.swift
+//  Friends
+//
+//  Created by Nathanael Youngren on 1/31/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBOutlet weak var imageOfFriend: UIImageView!
+    @IBOutlet weak var friendNameLabel: UILabel!
+    
+
+}
